be89eba16ae2da761a7ae119d38ad53a388f36a3
z46864417 <z46864417@gmail.com>
2024-03-08 14:42:09 +0800

    113-03-08

---
+++ android-studio-2024.3.8/app/src/main/java/com/example/android_studio_202438/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.android_studio_202438;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.PictureInPictureUiState;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void button_click(View view){
+        int count;
+        TextView output = (TextView) findViewById(R.id.textView);
+        count = Integer.parseInt(output.getText().toString());
+        count++;
+        output.setText(Integer.toString(count));
+    }
+
+    public void button2_click(View view){
+        TextView output = (TextView) findViewById(R.id.textView);
+        output.setText(R.string.ini_value);
+    }
+
+
+}
---
+++ android-studio-2024.3.8/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "android-studio-2024.3.8"
+include(":app")
+ 
